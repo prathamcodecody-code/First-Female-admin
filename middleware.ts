24762c7d7aa73314cd92a63910a82a97a926c379
prathamcodecody-code <pratham.codecody@gmail.com>
2025-12-18 20:21:28 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export function middleware(req) {
+export function middleware(req: NextRequest) {
   const token = req.cookies.get("admin_token")?.value || null;
 
   const protectedRoutes = [
