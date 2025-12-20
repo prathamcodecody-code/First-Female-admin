c9c9b60268d0074801f7286b502d84a7dd452bdc
prathamcodecody-code <pratham.codecody@gmail.com>
2025-12-20 12:22:35 +0530

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3030",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
 });
 
 api.interceptors.request.use(config => {
