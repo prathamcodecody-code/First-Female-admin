0d3b24a5308279b30f1c25a76a71e46f8fda2165
prathamcodecody-code <pratham.codecody@gmail.com>
2025-12-23 12:06:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,11 +12,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-white text-gray-900 antialiased">
+      <body className="min-h-screen bg-white text-gray-900 antialiased overflow-x-hidden">
         {/* App wrapper */}
-        <div className="min-h-screen flex">
+        
           {children}
-        </div>
+        
       </body>
     </html>
   );
