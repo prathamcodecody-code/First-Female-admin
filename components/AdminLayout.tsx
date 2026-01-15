4a3626317911eebe53cbe1b8c2e35197d56201f5
prathamcodecody-code <pratham.codecody@gmail.com>
2026-01-15 17:22:27 +0530

    Update AdminLayout.tsx

--- components/AdminLayout.tsx
+++ components/AdminLayout.tsx
@@ -1,14 +1,14 @@
-import dynamic from "next/dynamic";
+import AdminSidebarClient from "./AdminSidebarClient";
 
-const Sidebar = dynamic(() => import("./Sidebar"), {
-  ssr: false,
-  loading: () => null, // or small skeleton
-});
-
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="min-h-screen bg-white overflow-x-hidden">
-      <Sidebar />
+      <AdminSidebarClient />
+
       <main className="ml-0 md:ml-64 p-6 min-h-screen">
         {children}
       </main>
